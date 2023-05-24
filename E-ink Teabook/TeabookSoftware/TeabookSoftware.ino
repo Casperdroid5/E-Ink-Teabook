68c0f7635857d913f4f6a81b9f9034c523b32964
Casper <75725777+Casperdroid5@users.noreply.github.com>
2023-05-24 22:44:57 +0200

    Added project to Github

---
+++ E-ink Teabook/TeabookSoftware/TeabookSoftware.ino
@@ -0,0 +1,75 @@
+#include "Adafruit_ThinkInk.h"
+#include "Texts.h"
+#include <LowPower.h>
+
+// Pin definitions
+#define EPD_CS 10
+#define EPD_DC 9
+#define SRAM_CS 8
+#define EPD_RESET 5
+#define EPD_BUSY 3
+
+// Color settings
+#define BLACK EPD_BLACK
+#define RED EPD_RED
+
+
+//----Display selector----//
+
+//Flexible display
+//Adafruit_IL0373 display(212, 104, EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+//#define FLEXIBLE_213 // uncomment for flexible display size setting (required)
+
+// 2.13" Monochrome displays with 250x122 pixels and SSD1680 chipset
+ThinkInk_213_Mono_BN display(EPD_DC, EPD_RESET, EPD_CS, SRAM_CS, EPD_BUSY);
+
+// Time interval before new text is displayed
+const unsigned long DISPLAY_INTERVAL = 86400;  // One day: 86400
+
+const int ENAPin = 4;  // Ultra low power EPD pin
+
+char buffer[MAX_TEXT_LENGTH + 1];  // Buffer to store the retrieved text
+
+unsigned long previousDisplayTime = 0; 
+unsigned long interval = DISPLAY_INTERVAL;  // Desired interval in seconds
+
+void setup() {
+
+  display.begin();
+#if defined(FLEXIBLE_213) || defined(FLEXIBLE_290) // for flexible displays
+  display.setBlackBuffer(1, false);
+  display.setColorBuffer(1, false);
+#endif
+  drawimageEPD(getRandomText(), BLACK);
+  pinMode(EPD_RESET, OUTPUT);
+  pinMode(ENAPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousDisplayTime >= interval) {
+    previousDisplayTime = currentMillis;
+    drawimageEPD(getRandomText(), BLACK);
+  }
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); // go into sleep mode for extreme power saving
+}
+
+void drawimageEPD(const char* text, uint16_t color) {
+  digitalWrite(ENAPin, HIGH);  // Enable screen
+  display.clearBuffer();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.setTextColor(color);
+  display.setTextWrap(true);
+  display.print(text);
+  display.display();
+  digitalWrite(ENAPin, LOW);  // Disable screen
+}
+
+const char* getRandomText() {
+  int randomIndex = random(0, TEXT_COUNT);
+  strncpy_P(buffer, (char*)pgm_read_word(&(Texts[randomIndex])), MAX_TEXT_LENGTH);
+  buffer[MAX_TEXT_LENGTH] = '\0';
+  return buffer;
+}
---
+++ E-ink Teabook/TeabookSoftware/Texts.h
@@ -0,0 +1,145 @@
+#ifndef TEXTS_H
+#define TEXTS_H
+
+const int MAX_TEXT_LENGTH = 800;  // Maximum length of the text
+const int TEXT_COUNT = 65;        // Total number of texts in the array, match tis number to the amount of texts.
+
+#include <avr/pgmspace.h>
+// compliments/self help
+const char text0[] PROGMEM = "      \n\nYou look \n  beautiful today!";
+const char text1[] PROGMEM = "\n\n\n  Today is the day!";
+const char text2[] PROGMEM = "Nobody is perfect, \nbut you're so close \nit's scary";
+const char text3[] PROGMEM = "Your voice \nis my favorite sound";
+const char text4[] PROGMEM = "\n\nIf only you knew \nhow much those \nlittle moments with \nyou matter to me.";
+const char text5[] PROGMEM = "Every day with you \nis a wonderful \naddition to my \nlife's journey";
+const char text6[] PROGMEM = "Your smile is \nliterally \nthe cutest thing \nI've ever seen \nin my shelflife.";
+const char text7[] PROGMEM = "I know fairy tales \ncome true \n\nbecause I have you.";
+const char text8[] PROGMEM = "Everyone has their \nown motivation \nto get up in \nthe morning and \nface the day. \n\nyou are mine.";
+const char text9[] PROGMEM = "I know you might \nbe too busy today, \nbut please add me \nto your \nto-do list";
+const char text10[] PROGMEM = "\n\n  May you find \n  lots of reasons \n  to smile today!";
+const char text11[] PROGMEM = "I am who I am. \nNot who you \nthink I am. \nNot who you want me \nto be. \nI am me.";
+const char text12[] PROGMEM = "Seek to be whole, \nnot perfect.";
+const char text13[] PROGMEM = "You alone is enough. \nYou have nothing to \nprove to anybody.";
+const char text14[] PROGMEM = "Your mind is \njust as sexy \nas your beauty.";
+const char text15[] PROGMEM = "I Love your smile.";
+const char text16[] PROGMEM = "\n I miss your smile. \n\n         ;(";
+const char text17[] PROGMEM = "I love every inch \nof you \neven your toes.";
+const char text18[] PROGMEM = "I love your style";
+const char text19[] PROGMEM = "You're even more \nbeautiful \nin the morning";
+const char text20[] PROGMEM = "    \n\nYou have the \n  sweetest heart";
+const char text21[] PROGMEM = "I love looking \nat you";
+const char text22[] PROGMEM = "Some words when \nspoken \ncan't be \ntaken back";
+const char text23[] PROGMEM = "\n\n  On bended knee \n   is no way \n    to be free";
+const char text24[] PROGMEM = "The best revenge \nis to live on \nand prove yourself.";
+const char text25[] PROGMEM = "For you:  \n\n    ,d88b.d88b,\n    88888888888\n    `Y8888888Y'\n      `Y888Y'\n        `Y'\n \n   Love you!";  // ASCII heart art
+const char text26[] PROGMEM = "I know I was born\n and I know that \nI'll die \nThe in between \nis mine \nI am mine";
+const char text27[] PROGMEM = " You'll rise \n and fall. \n\n Take credit \n for both.";
+const char text28[] PROGMEM = "Hello? Anyone out \nthere? I've been \nleft open and now\n I feel like \n an open book.";
+const char text29[] PROGMEM = "Gee! \nIt sure feels good to \nstretch my pages again \nafter being stuck \non that cramped shelf \nfor ages.";
+const char text30[] PROGMEM = "Tee, hee. \nYou're tickling \nmy pages.";
+const char text31[] PROGMEM = "Are you cheating on me?\nI saw the way \nyou were looking at \nthat other \nEnglish romance novel.";
+const char text32[] PROGMEM = "Yes, \nI need to report \nsome missing pages. \nThey were last seen \nin the toddler's hands.";
+const char text33[] PROGMEM = "Hey, girl, \ndo you think we might \nbe reading too close \nto the water? \nCause I think we might \nbe reading too close \nto the water.";
+const char text34[] PROGMEM = "I'll always have \nyour back-cover";
+const char text35[] PROGMEM = "I can read you \nlike a book, \nwhich says a lot \n\ncomming from a book.";
+const char text36[] PROGMEM = "Every day I wait \nfor the moment \nto be opened \nand greeted \nby your beautifull \nsmile.";
+const char text37[] PROGMEM = "Please drink \na cuppa tea \nwith me";
+const char text38[] PROGMEM = "  Where there's tea, \n\n  there's happiness.";
+const char text39[] PROGMEM = "\n \n This is Tea-riffic!";
+const char text40[] PROGMEM = "If tea can't fix it, \nit's a serious problem.";
+const char text41[] PROGMEM = "Tea is a hug in a cup";
+const char text42[] PROGMEM = "Hit me with your \nbest pot.";
+const char text43[] PROGMEM = "I think there's a \ntea for that.";
+const char text44[] PROGMEM = "     Keep calm \n\n    and \n\nput the kettle on.";
+const char text45[] PROGMEM = "Rainy days should be \nspent at home with \na cup of tea \nand a good book.";
+const char text46[] PROGMEM = "Make tea, not war.";
+const char text47[] PROGMEM = "But first, \n\n     TEA.";
+const char text48[] PROGMEM = "Life is like a cup of tea,\n it's all in ho\nw you make it.";
+const char text49[] PROGMEM = "\n   Let's get this \n\n      par-tea \n\n         started!";
+const char text50[] PROGMEM = "The secret to a well \nbalanced life is a cup\n of tea in one hand\n and a good book\n in the other.";
+const char text51[] PROGMEM = "You can't buy \nhappiness, \nbut you can buy \ntea. \n\nand that's kind of \nthe same thing.";
+const char text52[] PROGMEM = "You, \nme, \nand a cup of tea.";
+const char text53[] PROGMEM = "Behind every successful \nwoman is a \nsubstantial \namount of tea.";
+const char text54[] PROGMEM = "Have a cup of \n\npositivi-tea.";
+const char text55[] PROGMEM = " I'd like to go \n   tea extra mile.";
+const char text56[] PROGMEM = "\n\n   This is my \n  mo-tea-vation.";
+const char text57[] PROGMEM = "Give love a chai.";
+const char text58[] PROGMEM = "    Be-leaf\n in your \n    abili-teas.";
+const char text59[] PROGMEM = "We have chemis-tea.";
+const char text60[] PROGMEM = "The right brew \nwill see \nyou through.";
+const char text61[] PROGMEM = "Tea in one hand. \nConfidence in the other";
+const char text62[] PROGMEM = "You are, \nand always will be, \nmy favourite \ntea drinking human";
+const char text63[] PROGMEM = "     .-. \n    (o o) \n    | O | \n   \\_-_/ \n    / | \\ \n   (_/ \\_)";
+const char text64[] PROGMEM = " /\\__/\\ \n(=^ .^=)   Three\nc   \"   c   \n /\\__/\\ \n(=^ .^=)   Cute\nc   \"   c\n /\\__/\\    Cats\n(=^ .^=)";
+;
+
+const char* const Texts[] PROGMEM = {
+  text0,
+  text1,
+  text2,
+  text3,
+  text4,
+  text5,
+  text6,
+  text7,
+  text8,
+  text9,
+  text10,
+  text11,
+  text12,
+  text13,
+  text14,
+  text15,
+  text16,
+  text17,
+  text18,
+  text19,
+  text20,
+  text21,
+  text22,
+  text23,
+  text24,
+  text25,
+  text26,
+  text27,
+  text28,
+  text29,
+  text30,
+  text31,
+  text32,
+  text33,
+  text34,
+  text35,
+  text36,
+  text37,
+  text38,
+  text39,
+  text40,
+  text41,
+  text42,
+  text43,
+  text44,
+  text45,
+  text46,
+  text47,
+  text48,
+  text49,
+  text50,
+  text51,
+  text52,
+  text53,
+  text54,
+  text55,
+  text56,
+  text57,
+  text58,
+  text59,
+  text60,
+  text61,
+  text62,
+  text63,
+  text64
+  
+};
+
+#endif
